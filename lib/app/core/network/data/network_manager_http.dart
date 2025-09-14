771e22c82421f5fdc37f385b8c3521d30b58d35b
Av3z <willyanf17@hotmail.com>
2025-09-14 00:16:38 -0300

    Refactor NetworkManagerHTTP methods to decode JSON responses and ensure proper async handling

--- lib/app/core/network/data/network_manager_http.dart
+++ lib/app/core/network/data/network_manager_http.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 import 'package:wayds_challenge/app/core/network/domain/network_manager.dart';
 
 class NetworkManagerHTTP extends NetworkManager {
@@ -9,24 +10,24 @@ class NetworkManagerHTTP extends NetworkManager {
   @override
   Future<T> delete<T>(String url, {Map<String, String>? headers}) async {
     final response = await http.delete(Uri.parse(url), headers: headers);
-    return response as T;
+    return jsonDecode(response.body);
   }
 
   @override
-  Future<T> get<T>(String url, {Map<String, String>? headers}) {
-    final response = http.get(Uri.parse(url), headers: headers);
-    return response as Future<T>;
+  Future<T> get<T>(String url, {Map<String, String>? headers}) async {
+    final response = await http.get(Uri.parse(url), headers: headers);
+    return jsonDecode(response.body);
   }
 
   @override
   Future<T> post<T>(String url, {Map<String, String>? headers, body}) async {
     final response = await http.post(Uri.parse(url), headers: headers, body: body);
-    return response as T;
+    return jsonDecode(response.body);
   }
 
   @override
   Future<T> put<T>(String url, {Map<String, String>? headers, body}) async {
     final response = await http.put(Uri.parse(url), headers: headers, body: body);
-    return response as T;
+    return jsonDecode(response.body);
   }
 }
