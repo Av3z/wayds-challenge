b6ae52065ddfc16164b17efce6e1d5dda718a0b0
Av3z <willyanf17@hotmail.com>
2025-09-14 11:15:57 -0300

    Add TColors class for centralized color management

---
+++ lib/app/design_system/theme/colors_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TColors {
+  static const Color primary = Color(0xFF6E30E8);
+  static const Color secondary = Color(0xFF00D6D6);
+  static const Color primaryDark = Color(0xFF4F26A2);
+  static const Color primaryLight = Color(0xFF9489FF);
+  static const Color darkGrey900 = Color(0xFF222222);
+  static const Color darkGrey800 = Color(0xFF343434);
+  static const Color darkGrey700 = Color(0xFF4D4D4D);
+  static const Color mediumGrey600 = Color(0xFF666666);
+  static const Color mediumGrey400 = Color(0xFF999999);
+  static const Color mediumGrey300 = Color(0xFFD9D9D9);
+  static const Color mediumGrey200 = Color(0xFFE6E6E6);
+  static const Color lightGrey100 = Color(0xFFF5F5F5);
+  static const Color black = Color(0xFF121212);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color success = Color(0xFF00BF4D);
+  static const Color redAlert = Color(0xFFF33333);
+  static const Color error = Color(0xFFF33333);
+}
