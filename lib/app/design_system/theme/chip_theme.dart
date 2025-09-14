fd221b032ea9de542519d6065b72217d62e158d9
Av3z <willyanf17@hotmail.com>
2025-09-14 11:12:22 -0300

    Add ChipThemeData for consistent chip styling

---
+++ lib/app/design_system/theme/chip_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:wayds_challenge/app/design_system/theme/export_theme.dart';
+
+final chipTheme = ChipThemeData(padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4), labelStyle: textTheme.labelSmall);
