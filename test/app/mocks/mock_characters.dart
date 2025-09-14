a575034247077eafbf9cb7e17ee82db4dd307a97
Av3z <willyanf17@hotmail.com>
2025-09-14 00:13:08 -0300

    Add mock data for characters in mock_characters.dart

---
+++ test/app/mocks/mock_characters.dart
@@ -0,0 +1,9 @@
+import 'package:wayds_challenge/app/core/character/domain/models/character.dart';
+
+final mockCharacters = [
+  Character(id: 1, name: 'Rick Sanchez', status: 'Alive', species: 'Human', type: '', image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg', gender: 'Male'),
+  Character(id: 2, name: 'Morty Smith', status: 'Alive', species: 'Human', type: '', image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg', gender: 'Male'),
+  Character(id: 3, name: 'Summer Smith', status: 'Alive', species: 'Human', type: '', image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg', gender: 'Female'),
+  Character(id: 4, name: 'Beth Smith', status: 'Alive', species: 'Human', type: '', image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg', gender: 'Female'),
+  Character(id: 5, name: 'Jerry Smith', status: 'Alive', species: 'Human', type: '', image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg', gender: 'Male'),
+];
