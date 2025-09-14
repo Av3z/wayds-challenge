c7d7b533823cc8281626f7b9e4769bd4d769ed63
Av3z <willyanf17@hotmail.com>
2025-09-14 12:11:43 -0300

    Add icon theme definition for consistent icon styling

---
+++ lib/app/design_system/theme/icon_theme.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:wayds_challenge/app/design_system/theme/export_theme.dart';
+
+final iconTheme = IconThemeData(color: TColors.darkGrey900, size: 24);
