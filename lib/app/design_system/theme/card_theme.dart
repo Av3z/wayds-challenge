01418f3010daf40d0c5b71b3074582110e2de4a2
Av3z <willyanf17@hotmail.com>
2025-09-14 12:12:49 -0300

    fix themes

--- lib/app/design_system/theme/card_theme.dart
+++ lib/app/design_system/theme/card_theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:wayds_challenge/app/design_system/theme/export_theme.dart';
 
 final cardTheme = CardThemeData(
-  color: Colors.white,
+  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+  color: TColors.white,
   elevation: 2,
   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
 );
