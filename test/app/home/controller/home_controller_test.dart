eb802e1177e27d24fe0770e93ccb9bfafe5ab2a2
Av3z <willyanf17@hotmail.com>
2025-09-14 22:22:49 -0300

    Refactor HomeController tests to use MockFetchCharactersUseCase and remove MockCharacterRepository

--- test/app/home/controller/home_controller_test.dart
+++ test/app/home/controller/home_controller_test.dart
@@ -1,21 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wayds_challenge/app/core/character/domain/models/character.dart';
 import 'package:wayds_challenge/app/home/controller/home_controller.dart';
-import 'package:wayds_challenge/app/core/character/domain/repositories/character_repository.dart';
 
-class MockCharacterRepository implements CharacterRepository {
-  final List<Character> mockCharacters;
-  MockCharacterRepository(this.mockCharacters);
-  @override
-  Future<List<Character>> fetchCharacters({int page = 1}) async => mockCharacters;
-}
+import '../../mocks/error_fetch_character_usecase.dart';
+import '../../mocks/mock_fetch_character_usecase.dart';
 
 void main() {
   test('HomeController fetchCharacters updates state', () async {
     final mockCharacters = [Character(id: 1, name: 'Rick', status: 'Alive', species: 'Human', type: '', image: '', gender: 'Male')];
-    final repository = MockCharacterRepository(mockCharacters);
-    final controller = HomeController(repository);
+    final usecase = MockFetchCharactersUseCase(mockCharacters);
+    final controller = HomeController(usecase);
     controller.fetchCharacters();
-    await expectLater(controller.stateStream, emits(isNotNull));
+    await Future.delayed(const Duration(milliseconds: 100));
+    expect(controller.isLoading.value, isFalse);
+    expect(controller.characters.value, mockCharacters);
+    expect(controller.hasError.value, isFalse);
+  });
+
+  test('HomeController sets error state on failure', () async {
+    final usecase = ErrorFetchCharactersUseCase();
+    final controller = HomeController(usecase);
+    controller.fetchCharacters();
+    await Future.delayed(const Duration(milliseconds: 100));
+    expect(controller.isLoading.value, isFalse);
+    expect(controller.hasError.value, isTrue);
   });
 }
