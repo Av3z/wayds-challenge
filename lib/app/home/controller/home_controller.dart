b6a5a7efbad555415a75333044a8373e3c15eb74
Av3z <willyanf17@hotmail.com>
2025-09-14 22:22:39 -0300

    Refactor HomeController to use CharacterBloc and update state management for character fetching

--- lib/app/home/controller/home_controller.dart
+++ lib/app/home/controller/home_controller.dart
@@ -1,21 +1,49 @@
-import 'dart:async';
-
-import 'package:wayds_challenge/app/core/character/domain/repositories/character_repository.dart';
-import 'package:wayds_challenge/app/home/state_manager/events/home_event.dart';
-import 'package:wayds_challenge/app/home/state_manager/state_manager.dart';
+import 'package:flutter/widgets.dart';
+import 'package:wayds_challenge/app/core/character/domain/models/character.dart';
+import 'package:wayds_challenge/app/core/character/domain/usecases/fetch_characters_usecase.dart';
+import 'package:wayds_challenge/app/home/state_manager/character_bloc.dart';
+import 'package:wayds_challenge/app/home/state_manager/events/character_event.dart';
+import 'package:wayds_challenge/app/home/state_manager/states/character_state.dart';
 
 class HomeController {
-  final StateManager stateManager;
+  final FetchCharactersUseCase repository;
+  late CharacterBloc characterBloc;
 
-  HomeController(CharacterRepository repository) : stateManager = StateManager(repository);
-
-  Stream get stateStream => stateManager.state.stream;
-
-  void fetchCharacters({int page = 1}) {
-    stateManager.dispatch(FetchCharactersEvent(page: page));
+  HomeController(this.repository) {
+    characterBloc = CharacterBloc(repository);
+    characterBloc.states.stream.listen((state) => onStateReceived(state));
   }
 
-  void dispose() {
-    stateManager.state.close();
+  ValueNotifier<List<Character>> characters = ValueNotifier([]);
+  ValueNotifier<bool> isLoading = ValueNotifier(false);
+  ValueNotifier<bool> hasError = ValueNotifier(false);
+
+  int currentPage = 1;
+  bool hasReachedMax = false;
+
+  void onStateReceived(CharacterState state) => switch (state) {
+    CharacterLoadingState() => onLoadingState(),
+    CharacterLoadedState() => onLoadedState(state),
+    CharacterErrorState() => onErrorState(),
+  };
+
+  void onLoadingState() {
+    isLoading.value = true;
+    hasError.value = false;
+  }
+
+  void onLoadedState(CharacterLoadedState state) {
+    isLoading.value = false;
+    characters.value.addAll(state.characters);
+    hasReachedMax = state.hasReachedMax;
+  }
+
+  void onErrorState() {
+    isLoading.value = false;
+    hasError.value = true;
+  }
+
+  void fetchCharacters({int page = 1}) async {
+    characterBloc.events.add(FetchCharactersEvent(page: page));
   }
 }
