d972bf2f6673a9ef7529d22dc8eb3b237a689847
Av3z <willyanf17@hotmail.com>
2025-09-14 22:22:53 -0300

    Refactor HomePage to use FetchCharactersUseCase and update tests to mock it

--- test/app/home/presentation/home_page_test.dart
+++ test/app/home/presentation/home_page_test.dart
@@ -5,15 +5,14 @@ import 'package:wayds_challenge/app/app_module.dart';
 import 'package:wayds_challenge/app/home/presentation/home_page.dart';
 
 import '../../mocks/mock_characters.dart';
-import 'package:wayds_challenge/app/core/character/domain/repositories/character_repository.dart';
 
+import '../../mocks/mock_fetch_character_usecase.dart';
 import '../../mocks/mock_shared_prefs.dart';
-import '../controller/home_controller_test.dart';
 
 void main() {
   setUp(() {
     Modular.bindModule(AppModule(MockSharedPrefs()));
-    Modular.replaceInstance<CharacterRepository>(MockCharacterRepository(mockCharacters));
+    Modular.replaceInstance<MockFetchCharactersUseCase>(MockFetchCharactersUseCase(mockCharacters));
   });
   testWidgets('should home page render correctly ', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: HomePage()));
